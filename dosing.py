7a90a9b1bb10abc885d56302068ca10b2114d421
Alana Gonzales <alana.gonzales@duke.edu>
2021-02-04 16:25:56 -0500

    improved refactoring for reuse and testing

--- dosing.py
+++ dosing.py
@@ -20,7 +20,8 @@ def get_input():
     print("Enter patient weight followed by units of kg or lb.")
     print("Examples:  65.3 lb      21.0 kg")
     weight_input = input("Enter weight: ")
-    data_manipulation(weight_input, diagnosis)
+    
+    return weight_input, diagnosis
 
 def data_manipulation(weight_input, diagnosis):
     ## Data manipulation
@@ -32,7 +33,8 @@ def data_manipulation(weight_input, diagnosis):
     dosages_mg_per_kg = [30, 10, 10, 12]
     dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
     dosage_mg_first_day = weight * dosage_mg_per_kg
-    do_output(dosage_mg_first_day, weight)
+    
+    return dosage_mg_first_day, weight
 
 def do_output(dosage_mg_first_day, weight):
     ## Output
@@ -41,7 +43,11 @@ def do_output(dosage_mg_first_day, weight):
     print("  the correct dosage is {:.1f} mg the first day"
           .format(dosage_mg_first_day))
 
+def dosing_function():
+    weight, diagnosis = get_input()
+    dosage_amount, weight = data_manipulation(weight, diagnosis)
+    do_output(dosage_amount, weight)
 
 if __name__ == '__main__':
-    get_input()
+    dosing_function()
 
